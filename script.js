9d28aa3966a0355b76edaf3a7a6d6e7cdad327f1
codycann <codycann@umich.edu>
2015-05-09 21:34:19 -0400

    Cross Script Messaging
    Attempts at cross script messaging

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+contentScriptMessage = "contentScriptMessage";
 var buttonOnSite = document.getElementById("player"),
     parent = buttonOnSite.parentElement,
     next = buttonOnSite.nextSibling,
@@ -6,5 +7,14 @@ var buttonOnSite = document.getElementById("player"),
 
 button.appendChild(text);
 button.className = "button";
+button.onclick=function(){
+	alert("Contentscript is sending a message to background script: '" + contentScriptMessage  + "'");
+	chrome.extension.sendRequest({message: contentScriptMessage});
+};
 if (next) parent.insertBefore(button, next);
 else parent.appendChild(button);
+
+chrome.runtime.onMessage.addListener(
+ function(request, sender) {
+  alert("Contentscript has received a message from from background script: '" + request.message + "'");
+  });
